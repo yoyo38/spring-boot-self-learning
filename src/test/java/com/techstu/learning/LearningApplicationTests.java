67259cce66dcec93fdec614107e9727e314695b6
Eric <hua152@163.com>
2019-11-29 23:45:04 +0800

    initial

---
+++ src/test/java/com/techstu/learning/LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techstu.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
